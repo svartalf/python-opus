ba750002f553b576544e4ff0fb6b1f40bf6725c2
SvartalF <self@svartalf.info>
2012-09-16 22:35:43 +0900

    `opus_encoder_get_size` function binding

--- opus/encoder.py
+++ opus/encoder.py
@@ -20,6 +20,17 @@ class Encoder(ctypes.Structure):
 
 EncoderPointer = ctypes.POINTER(Encoder)
 
+_get_size = libopus.opus_encoder_get_size
+_get_size.argtypes = (ctypes.c_int,)
+_get_size.restype = ctypes.c_int
+
+def get_size(channels):
+    """Gets the size of an OpusEncoder structure."""
+
+    if not channels in (1, 2):
+        raise ValueError('Wrong channels value. Must be equal to 1 or 2')
+
+    return _get_size(channels)
 
 _create = libopus.opus_encoder_create
 _create.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, c_int_pointer)
@@ -53,4 +64,7 @@ destroy.__doc__ = "Frees an OpusEncoder allocated by opus_encoder_create()"
 
 if __name__ == '__main__':
     encoder_state = create(12000, 2, constants.APPLICATION_VOIP)
+    print get_size(1)
+    print get_size(2)
+
     destroy(encoder_state)
