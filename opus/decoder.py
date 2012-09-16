1a00aeaecef7e549e4a5ff5f66ed058e03be5628
SvartalF <self@svartalf.info>
2012-09-16 23:08:03 +0900

    `opus_decoder_create` and `opus_decoder_destroy` function bindings

--- opus/decoder.py
+++ opus/decoder.py
@@ -5,6 +5,7 @@ from ctypes.util import find_library
 
 
 libopus = ctypes.CDLL(find_library('opus'))
+c_int_pointer = ctypes.POINTER(ctypes.c_int)
 
 
 class Decoder(ctypes.Structure):
@@ -16,6 +17,7 @@ class Decoder(ctypes.Structure):
     pass
 
 DecoderPointer = ctypes.POINTER(Decoder)
+
 
 _get_size = libopus.opus_decoder_get_size
 _get_size.argtypes = (ctypes.c_int,)
@@ -30,6 +32,34 @@ def get_size(channels):
     return _get_size(channels)
 
 
+_create = libopus.opus_decoder_create
+_create.argtypes = (ctypes.c_int, ctypes.c_int, c_int_pointer)
+_create.restype = DecoderPointer
+
+def create(fs, channels):
+    """Allocates and initializes a decoder state"""
+
+    if fs not in (8000, 12000, 16000, 24000, 48000):
+        raise ValueError('Wrong fs value. Must be equal to 8000, 12000, 16000, 24000 or 48000')
+
+    if not channels in (1, 2):
+        raise ValueError('Wrong channels value. Must be equal to 1 or 2')
+
+    result_code = ctypes.c_int()
+
+    result = _create(fs, channels, ctypes.byref(result_code))
+    # TODO: check for `result_code` value and raise an exception if needed
+
+    return result
+
+
+destroy = libopus.opus_decoder_destroy
+destroy.argtypes = (DecoderPointer,)
+destroy.restype = None
+destroy.__doc__ = 'Frees an OpusDecoder allocated by opus_decoder_create()'
+
 if __name__ == '__main__':
+    decoder = create(12000, 2)
     print get_size(1)
     print get_size(2)
+    destroy(decoder)
