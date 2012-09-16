62b674950a31c99bd1511e5ea6478805ac5f0606
SvartalF <self@svartalf.info>
2012-09-16 23:14:08 +0900

    License and setup.py files

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='opus',
+    version='0.0.1',
+    author='SvartalF',
+    author_email='self@svartalf.info',
+    url='https://github.com/svartalf/python-opus',
+    description='Python bindings to the libopus, IETF low-delay audio codec',
+    packages=('opus',),
+    classifiers=(
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+    ),
+)
